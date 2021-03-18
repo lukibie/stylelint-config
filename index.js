dbf74b6cfb01e8264002cb43de21561da7a5805c
Łukasz Bieniek <lukasz.bieniek@hotmail.com>
2021-03-18 17:39:35 +0100

    chore: simplify configuration
    BREAKING CHANGE: remove unnecessary plugins

--- index.js
+++ index.js
@@ -1,38 +1,12 @@
 module.exports = {
-  extends: [
-    "stylelint-config-standard",
-    "stylelint-config-rational-order",
-    "stylelint-config-prettier",
-  ],
-  plugins: [
-    "stylelint-scss",
-    "stylelint-order",
-    "stylelint-config-rational-order/plugin",
-  ],
+  extends: ["stylelint-config-standard", "stylelint-config-prettier"],
+  plugins: ["stylelint-order"],
   rules: {
-    "order/properties-order": [],
-    "plugin/rational-order": [
+    "order/properties-alphabetical-order": true,
+    "selector-type-no-unknown": [true, { ignoreTypes: ["/^lbui-/"] }],
+    "at-rule-no-unknown": [
       true,
-      {
-        "border-in-box-model": false,
-        "empty-line-between-groups": false,
-      },
+      { ignoreAtRules: ["/^lbui/", "/^define-mixin/", "/^mixin/"] },
     ],
-    "at-rule-no-unknown": null,
-    "unicode-bom": null,
-    "scss/at-rule-no-unknown": true,
-    "scss/at-extend-no-missing-placeholder": true,
-    "scss/at-function-named-arguments": "never",
-    "scss/at-function-parentheses-space-before": "never",
-    "scss/at-import-no-partial-leading-underscore": true,
-    "scss/at-mixin-parentheses-space-before": "never",
-    "scss/at-if-closing-brace-space-after": "always-intermediate",
-    "scss/at-else-empty-line-before": "never",
-    "scss/at-rule-conditional-no-parentheses": true,
-    "scss/dimension-no-non-numeric-values": true,
-    "scss/function-quote-no-quoted-strings-inside": true,
-    "scss/map-keys-quotes": "always",
-    // 'scss/at-function-pattern': '^gui-',
-    // 'scss/at-mixin-pattern': '^gui-',
   },
 };
